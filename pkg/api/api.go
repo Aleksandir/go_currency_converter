b96e3a35864aa327e28ab4e6b1264c3fb2ec2539
Aleks B <aleksandir@me.com>
2023-11-30 22:32:29 +1030

    Refactor conversion rate retrieval function

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -8,11 +8,16 @@ import (
 )
 
 type ConversionResponse struct {
-	Base       string             `json:"base"`
-	Date       string             `json:"date"`
-	Rates      map[string]float64 `json:"rates"`
+	Base  string             `json:"base"`
+	Date  string             `json:"date"`
+	Rates map[string]float64 `json:"rates"`
 }
 
+// GetConversionRate retrieves the conversion rate between two currencies.
+// It makes a GET request to the exchangerate-api.com API to fetch the latest rates.
+// The fromCurrency parameter specifies the currency to convert from.
+// The toCurrency parameter specifies the currency to convert to.
+// It returns the conversion rate as a float64 and an error if any.
 func GetConversionRate(fromCurrency string, toCurrency string) (float64, error) {
 	url := fmt.Sprintf("https://api.exchangerate-api.com/v4/latest/%s", fromCurrency)
 
