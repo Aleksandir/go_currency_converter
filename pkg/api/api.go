41586eca46b62b5f18f03a2a7f9727c58ea921d1
Aleks B <aleksandir@me.com>
2023-11-30 22:37:34 +1030

    Fix ioutil.ReadAll to io.ReadAll

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -3,7 +3,7 @@ package api
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 )
 
@@ -30,7 +30,7 @@ func GetConversionRate(fromCurrency string, toCurrency string) (float64, error) 
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return 0, err
 	}
